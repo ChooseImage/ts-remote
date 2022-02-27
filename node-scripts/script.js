cbedec8cef910fae00032728548d862d2ee2dd17
Billy Ho <choose0125@gmail.com>
2022-02-26 19:58:35 -0500

    probing-qs

--- node-scripts/script.js
+++ node-scripts/script.js
@@ -10,11 +10,11 @@ const { Configuration, OpenAIApi } = require("openai");
 let ui = null;
 let prompt = null;
 let userInput;
-let  numToken = 2;
+let  numToken = 120;
 let nTimesPressed = 0;
-let prompt01 = "First_Prompt";
+let prompt01 = "A: When was this?\nB: Sunday afternoon I think, or is it Saturday?\nA: Were you alone?\nB: No, I remember seeing my mom, in the park, but younger.\n";
 let userName = 'user';
-let botName = 'Callaway';
+let botName = 'A:';
 let convo = null;
 
 // Place to store all the priming dreams
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // const p1 = document.createElement('div');
 
   // p1.innerHTML = initPrompt;
-  //Lprimes.insertAdjacentElement("afterbegin", p1);
+  // convo.insertAdjacentElement("afterbegin", p1);
 
 
 
@@ -93,7 +93,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
     //get prompt
     //condition ? exprIfTrue : exprIfFalse
-    userInput = `U: ${input}`;
+    userInput = `${prompt01}A: ${input}`;
 
     console.log(nTimesPressed);
     
@@ -104,7 +104,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
     modelPrompt = primeMats + userInput;
 
+
+    // Remove user input box
+
+    let elem = document.getElementById(`Linput${nTimesPressed}`);
+    elem.parentElement.removeChild(elem);
+
+    // Insert userInput as new HTML
+
+    const p2 = document.createElement('div');
+
+    p2.innerHTML = input;
+    convo.insertAdjacentElement("beforeend", p2);
+
     nTimesPressed++;
+
+  
     
     makecall(userInput);
 
@@ -126,13 +141,15 @@ const addField = () => {
   // This goes in makecall(), need to wait the site to render the response first
   
   let html =`
-  <input type="text" id="Linput${nTimesPressed}" name="userPrime">
+  <textarea name="Text1" cols="40" rows="5" id="Linput${nTimesPressed}"></textarea>
                 `
 
   const p = document.createElement('div');
   p.innerHTML = html;
   convo.appendChild(p);
 }
+
+
 
 
 const makecall = (async (prompt) => {
@@ -145,23 +162,55 @@ const makecall = (async (prompt) => {
           model:"davinci:ft-personal-2022-02-22-23-16-53",
           prompt: prompt,
           max_tokens:numToken,
-          stop: '\n'
+          stop: ["A:"]
         });
-  
+        
+       // console.log(data);
         console.log(prompt+' '+response.data.choices[0].text);
-        let data = response.data.choices[0].text;
-        data.replace('#', '');
+        let data = response.data.choices[0].text.replace('###', '.\n').replace('A:', '').replace('B:', '');
+        // data.replace('###', '');
+        // data.replace('A:', '');
         //.replace('characterToReplace', '');
         // data.replace(((nTimesPressed%2==1) ? "A:" : "B:"), '');
         let displayText = `${botName}: ${data}`;
+        displayText = data;
 
         let html = `
-            <div>
+            <div class='gentext'>
                 <p>${displayText}</p>
             </div>
             `;
 
         // return html;
+
+
+        /*
+
+        TYPEWRITER WORK IN PROGRESS
+
+        const typeWriter = (txt, id)=> {
+
+          let i = 0;
+          let speed = 50;
+          if (i < txt.length) {
+            document.getElementById(id).innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+          }
+        }
+         setTimeout(typeWriter, 500);
+         typeWriter(displayText, 'valueInput');
+        
+          let i = 0;
+          let speed = 50;
+          if (i < txt.length) {
+            document.getElementById(id).innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+          }
+         */
+
+
   
         const p = document.createElement('div');
         //"clearcontent('clear')
@@ -170,7 +219,9 @@ const makecall = (async (prompt) => {
         convo.appendChild(p);
 
         addField();
-
+        console.log(data);
+        return(data);
+        //typeWriter(displayText, 'valueInput');
   });
   //makecall("Who's there?");
 //console.log(document.querySelector('.ui').textContent);
