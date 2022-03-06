485724f3d5d1070fcaad264f722d70aa14bde1e3
Billy Ho <choose0125@gmail.com>
2022-03-06 17:29:02 -0500

    cache ailog

--- node-scripts/call.js
+++ node-scripts/call.js
@@ -1,8 +1,9 @@
 /* 
 
 Buttons for field and processing call requests
+line by line chatbot 
 
-todo: Multiple stops, need to add '\nB'
+TODO: Cache current stage and pass to priming mats.
 
 */
 
@@ -12,37 +13,72 @@ let ui = null;
 let prompt = null;
 let userInput;
 let  numToken = 30;
-let nTimesPressed = 1;
+let nTimesPressed = 0;
+let prompt01 = "A: When was this?\nB: Sunday afternoon I think, or is it Saturday?\nA: Were you alone?\nB: No, I remember seeing my mom, in the park, but younger.\n";
+let userName = 'user';
+let botName = 'A:';
+let convo = null;
 
 // Place to store all the priming dreams
 let primeMats='';
+let dialog = prompt01; //store all the on going dialog.
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
 
 
+
+
+//nameInput();
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
 
   ui = document.querySelector('.ui');
   primes = document.querySelector('#primes');
+  convo = document.querySelector('.convo');
+  //Lprimes = document.querySelector('#Lprimes');
+
+  // set/store user name
+
+  //userName = window.prompt("Please enter your name:", "User");
+
+  // The first prompt on load
+
+
+  let initPrompt = `
+    <div>
+    <p>${prompt01}</p>
+    </div>
+  `
+
+  // ------------------------------------------------------------- // 
+
+  // const p1 = document.createElement('div');
+
+  // p1.innerHTML = initPrompt;
+  // convo.insertAdjacentElement("afterbegin", p1);
+
 
 
   // btn handling adding fields 
 
-  document.querySelector('#btn-addField').addEventListener('click',()=>{
+  //document.querySelector('.btn').addEventListener('click',()=>{
     //Add field btn function
-    nTimesPressed++;
-    let field = `
-    </br>
-      <input type="text" id="userPrimes${nTimesPressed}" name="userPrime">
-    `;
-
-    let inputField = document.createElement('div');
-    inputField.innerHTML = field;
-    primes.appendChild(inputField);
     
-  });
+  //   let field = `
+  //   </br>
+  //     <input type="text" id="userPrimes${nTimesPressed}" name="userPrime">
+  //   `;
+
+  //   let inputField = document.createElement('div');
+  //   inputField.innerHTML = field;
+  //   primes.appendChild(inputField);
+    
+    
+  // });
 
 
     /*
@@ -50,55 +86,92 @@ document.addEventListener('DOMContentLoaded', function () {
      making calls 
      */
 
-  document.querySelector('#btn').addEventListener('click',()=>{
+  document.querySelector(`#Lbtnsubmit0`).addEventListener('click',()=>{
 
-    //store user primes if there's any
-    //const up1 = document.getElementById('userPrime1').value;
+    // collect the first input and make request 
     
-    //concact primes
-    //Some loop to collect all the input values there is.
 
-    for(let i =1; i<= nTimesPressed; i++){
-      let id = `userPrimes${i}`
+      let id = `Linput${nTimesPressed}`
       let input = document.getElementById(`${id}`).value;
-      if(i%2==1){
-        primeMats+= 'A:' + input + '\n';
-      }else{
-        primeMats += "B:" + input + '\n';
-      }
+      let INPUT = `
+        <div class = 'userArea'>
+          <p>${input}</p>
+        </div>
+      `
 
-     
-    }
-
+      dialog += nTimesPressed==0 ? `A: ${input}`: `\nA: ${input}`;
 
 
-  
-
-  
+    
+    console.log(`DIALOG-00: \n${dialog}`);
     //get prompt
     //condition ? exprIfTrue : exprIfFalse
+    userInput = `${prompt01}A: ${input}`;
 
-    userInput = (nTimesPressed%2==1) ? "B:" : "A:"  + document.getElementById('prompt').value;
+    //console.log(nTimesPressed);
     
-    console.log("prompt: " + document.getElementById('prompt').value);
-    console.log("-------------------------");
+    //console.log("prompt: " + document.getElementById('prompt').value);
+    //console.log("-------------------------");
 
     //combine user Priming and prompts 
 
     modelPrompt = primeMats + userInput;
-    console.log(modelPrompt);
+
+
+    // Remove user input box
+
+    let elem = document.getElementById(`Linput${nTimesPressed}`);
+    elem.parentElement.removeChild(elem);
+
+    // Insert userInput as new HTML
+
+    const p2 = document.createElement('div');
+
+    p2.innerHTML = INPUT;
+    convo.insertAdjacentElement("beforeend", p2);
+
+    nTimesPressed++;
+
+  
     
-    console.log(primeMats);
+    makecall(dialog);
+    console.log(`Prompt-${nTimesPressed}: \n${dialog}`);
 
-    makecall(modelPrompt);
+   
 
-    primeMats = [];
+    console.log(`call made, nTimePressed = ${nTimesPressed}`);
+    
+
+    //primeMats = [];
 
   });
 
 });
 
 const openai = new OpenAIApi(configuration);
+
+const addField = () => {
+
+  // This goes in makecall(), need to wait the site to render the response first
+  
+  let html =`
+  <textarea name="Text1" cols="40" rows="5" id="Linput${nTimesPressed}"></textarea>
+                `
+
+  const p = document.createElement('div');
+  p.innerHTML = html;
+  convo.appendChild(p);
+}
+
+/*
+let text = "Hello world, welcome to the universe.";
+let result = text.indexOf("welcome");
+
+text.slice(0, result);
+
+*/
+
+
 
 
 const makecall = (async (prompt) => {
@@ -111,29 +184,77 @@ const makecall = (async (prompt) => {
           model:"davinci:ft-personal-2022-02-22-23-16-53",
           prompt: prompt,
           max_tokens:numToken,
-          stop: '\n'
+          stop: ["A"]
         });
-  
+        
+       // console.log(data);
         //console.log(prompt+' '+response.data.choices[0].text);
-        let data = response.data.choices[0].text;
-
+        let data = response.data.choices[0].text.replace('###', '').replace('A:', '').replace('B:', '');
+        // data.replace('###', '');
+        // data.replace('A:', '');
         //.replace('characterToReplace', '');
-        data.replace(((nTimesPressed%2==1) ? "A:" : "B:"), '');
+        // data.replace(((nTimesPressed%2==1) ? "A:" : "B:"), '');
+        let rawData = response.data.choices[0].text;
+        let displayText = `${botName}: ${data}`;
+        displayText = data;
 
         let html = `
-            <div>
-                <p>${data}</p>
+            <div class='gentext'>
+                <p>${displayText}</p>
             </div>
             `;
+        
+        let index = data.indexOf('B:');
 
+        if(index >0){
+          data.slice(0, index);
+        }
+
+        console.log('INDEX: ' + index);
+        
         // return html;
+        console.log(`test-> ${response.data.choices[0].text}`);
+        dialog+= rawData;
+        console.log(`DIALOG-01 \n${dialog}`)
+
+        /*
+
+        TYPEWRITER WORK IN PROGRESS
+
+        const typeWriter = (txt, id)=> {
+
+          let i = 0;
+          let speed = 50;
+          if (i < txt.length) {
+            document.getElementById(id).innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+          }
+        }
+         setTimeout(typeWriter, 500);
+         typeWriter(displayText, 'valueInput');
+        
+          let i = 0;
+          let speed = 50;
+          if (i < txt.length) {
+            document.getElementById(id).innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+          }
+         */
+
+
   
         const p = document.createElement('div');
         //"clearcontent('clear')
         
         p.innerHTML = html;
-        ui.appendChild(p);
+        convo.appendChild(p);
 
+        addField();
+        //console.log(data);
+        return(data);
+        //typeWriter(displayText, 'valueInput');
   });
   //makecall("Who's there?");
 //console.log(document.querySelector('.ui').textContent);
